91200a187eb1d6470fcbcde45ffb0d381cd48493
박준서 <pppg12jintpt@gmail.com>
2025-10-19 17:45:24 +0900

    add asset

---
+++ A1/Source/A1/Character/A1Pawn.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/A1Pawn.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+// Sets default values
+AA1Pawn::AA1Pawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	Capsule->InitCapsuleSize(34.0f, 100.0f);
+
+	SetRootComponent(Capsule);
+
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Capsule);
+	Mesh->SetRelativeLocation(FVector(0.0f, 0.0f, -100.0f));
+	Mesh->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Game/_Art/ParagonGreystone/Characters/Heroes/Greystone/Meshes/Greystone.Greystone'"));
+
+	if (MeshAsset.Succeeded())
+	{
+		Mesh->SetSkeletalMesh(MeshAsset.Object);
+	}
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->TargetArmLength = 700.0f;
+	SpringArm->SetRelativeRotation(FRotator(-30.0f, 0.0f, 0.0f));
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
+
+// Called when the game starts or when spawned
+void AA1Pawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AA1Pawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AA1Pawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
